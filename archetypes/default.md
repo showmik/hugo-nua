c566194de0cbef4df92f3ad2ad7b679a05e8338c
Intisar Bin Naim <showmik.haque@gmail.com>
2024-10-03 16:18:43 +0600

    improves overall features and codes

--- archetypes/default.md
+++ archetypes/default.md
@@ -2,4 +2,7 @@
 title = '{{ replace .File.ContentBaseName "-" " " | title }}'
 date = {{ .Date }}
 draft = true
+author = "{{ .Site.Params.author.name }}"
+description = "Description of the content."
+tags = []
 +++
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    renderMathInElement(document.body, {
-      delimiters: [
-        {left: "$$", right: "$$", display: true},  // Block math
-        {left: "$", right: "$", display: false}    // Inline math
-      ]
-    });
+    if (typeof renderMathInElement === "function") {
+      try {
+        renderMathInElement(document.body, {
+          delimiters: [
+            {left: "$$", right: "$$", display: true},
+            {left: "$", right: "$", display: false}
+          ]
+        });
+      } catch (error) {
+        console.error("Error rendering math:", error);
+      }
+    } else {
+      console.warn("renderMathInElement function is not available.");
+    }
   });
   
